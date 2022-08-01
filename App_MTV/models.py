9145d801f2e28674d6313d3a449f751d3fc397b0
Valentin Vecino <109243833+31012000@users.noreply.github.com>
2022-08-01 16:05:51 -0300

    Se agregaron modelos a la base de datos

--- App_MTV/models.py
+++ App_MTV/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class Sucursal(models.Model):
+    direccion = models.CharField(max_length=50)
+    horario = models.DateField()
+    cantidad_empleados = models.IntegerField()
+
+
+class Comida(models.Model):
+    marca = models.CharField(max_length=50)
+    kilogramos = models.IntegerField()
+    tipo_comida = models.CharField(max_length=50)
+
+class Clientes(models.Model):
+    nombre = models.CharField(max_length=50) 
+    apellido = models.CharField(max_length=50)    
+    apellido = models.EmailField()   
+    nombre_mascota = models.CharField(max_length=50)
+
+
+
+class Direccion(models.Model):
+    direccion = models.CharField(max_length=50)
